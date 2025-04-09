e16f427cb2fcc721be27e676fb7773e1dc1e6616
eunoia73 <soo97703@naver.com>
2025-04-09 20:32:39 +0900

    feat: #3 임시 토큰으로 테스트해보기

--- src/main/java/org/example/jwtserver/filter/MyFilter3.java
+++ src/main/java/org/example/jwtserver/filter/MyFilter3.java
@@ -1,15 +1,35 @@
 package org.example.jwtserver.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        log.info("필터3");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        req.setCharacterEncoding("UTF-8");
+        //토큰 : cos
+        if (req.getMethod().equals("POST")) {
+            String headerAuth = req.getHeader("Authorization");
+            log.info("Authorization Header : {}", headerAuth);
+            log.info("필터1");
+
+            if (headerAuth.equals("cos")) {
+                filterChain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증 안 됨");
+                log.info("인증 안됨");
+            }
+        }
     }
 }
